1b0b9e0ffda3961865e1e4c228751914589d6038
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-10-05 15:40:11 +0900

    verify environment

--- run.sh
+++ run.sh
@@ -23,8 +23,15 @@ if [ ! -n "$WERCKER_ERRBIT_NOTIFY_ENVIRONMENT" ]; then
   environment="production"
 fi
 
-repository=git@$WERCKER_GIT_DOMAIN:$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY\.git
-info $repository
+if [ -n "$WERCKER_GIT_DOMAIN" ]; then
+  scm_repository=git@$WERCKER_GIT_DOMAIN:$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY\.git
+fi
+info $scm_repository
+
+if [ -n "$WERCKER_GIT_COMMIT" ]; then
+  scm_revision=$WERCKER_GIT_COMMIT
+fi
+info $scm_revision
 
 wercker_url=https://app.wercker.com/#deploy/$WERCKER_DEPLOY_ID
 info $wercker_url
@@ -33,7 +40,7 @@ $cmd -G \
   --data-urlencode api_key=$WERCKER_ERRBIT_NOTIFY_API_KEY \
   --data-urlencode deploy\[local_username\]=$WERCKER_STARTED_BY \
   --data-urlencode deploy\[rails_env\]=$environment \
-  --data-urlencode deploy\[scm_repository\]=$repository \
-  --data-urlencode deploy\[scm_revision\]=$WERCKER_GIT_COMMIT \
+  --data-urlencode deploy\[scm_repository\]=$scm_repository \
+  --data-urlencode deploy\[scm_revision\]=$scm_revision \
   --data-urlencode deploy\[message\]=$wercker_url \
   $schema://$WERCKER_ERRBIT_NOTIFY_HOST/deploys.txt
