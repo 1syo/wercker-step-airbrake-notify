b2cec83ca7c1eb3e6f7f16d174f4244bbbe3d7d5
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-10-10 00:53:16 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ if [ ! -n "$DEPLOY" ]; then
   fail 'Please set deploy after steps only.'
 fi
 
+if [ "$WERCKER_RESULT" != "passed" ]; then
+  info "deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY failed."
+  return 0
+fi
+
 if [ ! -n "$WERCKER_AIRBRAKE_NOTIFY_API_KEY" ]; then
   fail 'Please specify api key property.'
 fi
@@ -13,12 +18,11 @@ if [ ! -n "$cmd" ]; then
   fail 'Command not found.'
 fi
 
-if [ ! -n "$WERCKER_AIRBRAKE_NOTIFY_HOST" ]; then
+if [ -n "$WERCKER_AIRBRAKE_NOTIFY_HOST" ]; then
   host="$WERCKER_AIRBRAKE_NOTIFY_HOST"
 else
   host="airbrake.io"
 fi
-
 
 if [ -n "$WERCKER_AIRBRAKE_NOTIFY_SECURE" ]; then
   test "$WERCKER_AIRBRAKE_NOTIFY_SECURE" = "true" && schema="https" || schema="http"
@@ -31,25 +35,34 @@ if [ ! -n "$WERCKER_AIRBRAKE_NOTIFY_ENVIRONMENT" ]; then
 else
   environment="$WERCKER_AIRBRAKE_NOTIFY_ENVIRONMENT"
 fi
+info $environment
+
 
 if [ -n "$WERCKER_GIT_DOMAIN" ]; then
   scm_repository=git@$WERCKER_GIT_DOMAIN:$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY\.git
 fi
 info $scm_repository
 
+
 if [ -n "$WERCKER_GIT_COMMIT" ]; then
   scm_revision=$WERCKER_GIT_COMMIT
 fi
 info $scm_revision
 
-wercker_url="https://app.wercker.com/#deploy/$WERCKER_DEPLOY_ID"
-info $wercker_url
+
+if [ -n "$WERCKER_AIRBRAKE_NOTIFY_MESSAGE" ]; then
+  message="$WERCKER_AIRBRAKE_NOTIFY_MESSAGE"
+else
+  message="Deploy to $WERCKER_DEPLOYTARGET_NAME passed."
+fi
+info "$message"
+
 
 $cmd -G \
-  --data-urlencode api_key=$WERCKER_AIRBRAKE_NOTIFY_API_KEY \
-  --data-urlencode deploy\[local_username\]=$WERCKER_STARTED_BY \
-  --data-urlencode deploy\[rails_env\]=$environment \
-  --data-urlencode deploy\[scm_repository\]=$scm_repository \
-  --data-urlencode deploy\[scm_revision\]=$scm_revision \
-  --data-urlencode deploy\[message\]=$wercker_url \
-  "$schema://$WERCKER_AIRBRAKE_NOTIFY_HOST/deploys.txt"
+  --data-urlencode "api_key=$WERCKER_AIRBRAKE_NOTIFY_API_KEY" \
+  --data-urlencode "deploy[local_username]=$WERCKER_STARTED_BY" \
+  --data-urlencode "deploy[rails_env]=$environment" \
+  --data-urlencode "deploy[scm_repository]=$scm_repository" \
+  --data-urlencode "deploy[scm_revision]=$scm_revision" \
+  --data-urlencode "deploy[message]=$message" \
+  "$schema://$host/deploys.txt" -o '' 2>/dev/null
