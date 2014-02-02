63fcb8cc835b33fd7f798009eb2dcb465acbc441
TAKAHASHI Kazunari <takahashi@1syo.net>
2014-02-02 22:18:21 +0900

    evaluate http status code

--- run.sh
+++ run.sh
@@ -1,21 +1,13 @@
-#!/bin/sh
-
 if [ ! -n "$DEPLOY" ]; then
   fail 'Please set deploy after steps only.'
 fi
 
 if [ "$WERCKER_RESULT" != "passed" ]; then
-  info "deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY failed."
-  return 0
+  fail "deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY failed."
 fi
 
 if [ ! -n "$WERCKER_AIRBRAKE_NOTIFY_API_KEY" ]; then
   fail 'Please specify api key property.'
-fi
-
-cmd=`which curl`
-if [ ! -n "$cmd" ]; then
-  fail 'Command not found.'
 fi
 
 if [ -n "$WERCKER_AIRBRAKE_NOTIFY_HOST" ]; then
@@ -37,18 +29,15 @@ else
 fi
 info $environment
 
-
 if [ -n "$WERCKER_GIT_DOMAIN" ]; then
   scm_repository=git@$WERCKER_GIT_DOMAIN:$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY\.git
 fi
 info $scm_repository
 
-
 if [ -n "$WERCKER_GIT_COMMIT" ]; then
   scm_revision=$WERCKER_GIT_COMMIT
 fi
 info $scm_revision
-
 
 if [ -n "$WERCKER_AIRBRAKE_NOTIFY_MESSAGE" ]; then
   message="$WERCKER_AIRBRAKE_NOTIFY_MESSAGE"
@@ -57,11 +46,20 @@ else
 fi
 info "$message"
 
-$cmd -G -s \
+result=`curl -G -s \
   --data-urlencode "api_key=$WERCKER_AIRBRAKE_NOTIFY_API_KEY" \
   --data-urlencode "deploy[local_username]=$WERCKER_STARTED_BY" \
   --data-urlencode "deploy[rails_env]=$environment" \
   --data-urlencode "deploy[scm_repository]=$scm_repository" \
   --data-urlencode "deploy[scm_revision]=$scm_revision" \
   --data-urlencode "deploy[message]=$message" \
-  "$schema://$host/deploys.txt" > /dev/null
+  "$schema://$host/deploys.txt" \
+  --output "$WERCKER_STEP_TEMP/result.txt" \
+  --write-out "%{http_code}"`
+
+if [ "$result" = "200" ]; then
+  success "Finished successfully!"
+else
+  echo -e "`cat $WERCKER_STEP_TEMP/result.txt`"
+  fail "Finished Unsuccessfully."
+fi
